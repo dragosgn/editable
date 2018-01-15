b0cdcf850c651562eec744387295167426a41b94
Dragos Nedelcu <dragosgn@gmail.com>
2018-01-15 11:20:03 +0100

    basic scafolding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react"
+import {compose} from "recompose"
 
+export default(WrappedComponent) => (
+  return class extends React.Component {
+    constructor(props){
+      super(props)
+      this.enterEdit = this.enterEdit.bind(this)
+    }
 
-export default() => (
-  <div>
+    enterEdit() {
 
-  </div>
+    }
+
+    render() {
+      return(
+        <WrappedComponent {...this.props} enterEdit={this.enterEdit}>
+      )
+    }
+  }
 )
