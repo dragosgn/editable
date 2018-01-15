0d597d6425bb261f9df902bb3630410861e5d85d
Dragos Nedelcu <dragosgn@gmail.com>
2018-01-15 11:27:41 +0100

    corrected inline function mistakes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 import React from "react"
 
-export default(WrappedComponent) => (return class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      edit: false
+export default(WrappedComponent) => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        edit: false
+      }
+
+      this.enterEdit = this.enterEdit.bind(this)
+      this.exitEdit = this.exitEdit.bind(this)
     }
 
-    this.enterEdit = this.enterEdit.bind(this)
-    this.exitEdit = this.exitEdit.bind(this)
-  }
-
-  enterEdit() {
-    this.setState({
-      edit: true
-    })
-  }
-
-  exitEdit() {
-    this.setState({
-      edit: false
-    })
-  }
-
-  render() {
-    return (<WrappedComponent {...this.props} enterEdit={this.enterEdit}>
-      )
+    enterEdit() {
+      this.setState({edit: true})
     }
-  }
-)
+
+    exitEdit() {
+      this.setState({edit: false})
+    }
+
+    render() {
+      return (<WrappedComponent {...this.props} enterEdit={this.enterEdit}>
+        )
+      }
+    }
+}
